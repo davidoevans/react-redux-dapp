7b651504bf4196584f51e221a4317f3113454b79
David Evans <david.evans@e-canada.io>
2017-02-04 15:56:56 -0500

    update to use web3 provided (although still can't use MetaMask web3 as provided do to non-async calls in views

--- src/components/reflux/AccountStore.jsx
+++ src/components/reflux/AccountStore.jsx
@@ -2,31 +2,42 @@ import Reflux from 'reflux';
 import Actions from './actions.jsx';
 import Web3 from 'web3';
 
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+// import truffleConfig from '../../../truffle.js';
+
+// var web3Location = `http://${truffleConfig.rpc.host}:${truffleConfig.rpc.port}`
+var web3Location = 'http://localhost:8545';
+var web3Provided;
+// Supports Metamask and Mist, and other wallets that provide 'web3'.
+if (typeof web3 === 'undefined') {
+  // Use the Mist/wallet provider.
+  // eslint-disable-next-line
+  web3Provided = new Web3(web3.currentProvider);
+} else {
+  web3Provided = new Web3(new Web3.providers.HttpProvider(web3Location))
+}
 
 var AccountStore = Reflux.createStore({
   listenables: [Actions],
   getAccounts: function() {
-    let _accounts = [];
     //web3.eth.accounts.map(function(account) {
-    var addresses = web3.eth.accounts;
-    for (var i = 0; i < addresses.length; i++) {
-      _accounts.push({address: addresses[i],
-        balance: web3.fromWei(web3.eth.getBalance(addresses[i]), 'ether')
-      });
-    };
-    this.accounts = _accounts;
-    this.fireUpdate();
+    web3Provided.eth.getAccounts(function(error, addresses) {
+      let _accounts = [];
+      for (var i = 0; i < addresses.length; i++) {
+        _accounts.push({address: addresses[i], balance: web3Provided.fromWei(web3Provided.eth.getBalance(addresses[i]), 'ether') })
+      };
+      this.accounts = _accounts;
+      this.fireUpdate();
+    }.bind(this));
   },
   getAddressList: function() {
-    this.addressList = web3.eth.accounts;
+    this.addressList = web3Provided.eth.accounts;
     // this.addressList = _accounts.map(function(account) {
     //   return {address: account};
     // })
     this.trigger('change', this.addressList);
   },
   getBalance: function(_account) {
-    this.balance = web3.fromWei(web3.eth.getBalance(_account), 'ether');
+    this.balance = web3Provided.fromWei(web3Provided.eth.getBalance(_account), 'ether');
     this.trigger('change', this.balance);
   },
   fireUpdate: function() {
