5376ae03453c7cc2aa1813c9a7852d7e227a6401
David Evans <david.evans@e-canada.io>
2017-02-04 15:53:00 -0500

    added development network to truffle config

--- truffle.js
+++ truffle.js
@@ -1,7 +1,12 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
 module.exports = {
-  rpc: {
-    host: 'localhost',
-    port: 8545
-  },
-  "migrations_directory": "./migrations"
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    }
+  }
 }
