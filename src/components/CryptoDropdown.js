949254b178c540d98783e3efe3c2df82cd138ce4
David Evans <david.evans@e-canada.io>
2017-03-26 14:40:59 -0400

    Revert "updated image for documentation"
    This reverts commit de6b2b26414dfd46069d170761e457d95c67b589.

--- src/components/CryptoDropdown.js
+++ src/components/CryptoDropdown.js
@@ -6,7 +6,6 @@ const CryptoDropdown = ({ label, cryptos, action }) => (
     <div className="col-sm-10">
       <select className="form-control" onChange={
           (e) => action(e.target.value)}>
-        <option value="" selected disabled>Select Currency</option>
         {cryptos.map(crypto =>
           <option key={crypto.id}>{crypto.symbol}</option>
           )
