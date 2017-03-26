2aaca42baa20fc575de64e6f1361d48ec30067bd
David Evans <david.evans@e-canada.io>
2017-03-26 14:39:01 -0400

    use form-group-sm styling, add selected disabled for default on dropdowns

--- src/components/AddressDropdown.js
+++ src/components/AddressDropdown.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const AddressDropdown = ({ label, accounts, action }) => (
-  <div className="form-group">
+  <div className="form-group form-group-sm">
     <label className="col-sm-2 control-label">{label}</label>
     <div className="col-sm-10">
       <select className="form-control" onChange={
           (e) => action(e.target.value)}>
+        <option value="" selected disabled>Select Account</option>
         {accounts.map(account =>
           <option key={account.address}>{account.address}</option>
           )
--- src/components/CryptoDropdown.js
+++ src/components/CryptoDropdown.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const CryptoDropdown = ({ label, cryptos, action }) => (
-  <div className="form-group">
+  <div className="form-group form-group-sm">
     <label className="col-sm-2 control-label">{label}</label>
     <div className="col-sm-10">
       <select className="form-control" onChange={
