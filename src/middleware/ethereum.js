43851e4361e2279b57e1e37b9559c33f2334c7bb
David Evans <david.evans@e-canada.io>
2017-04-03 07:14:49 -0400

    remove hardcoded identity item

--- src/middleware/ethereum.js
+++ src/middleware/ethereum.js
@@ -6,19 +6,6 @@ import { getBalance, sendCoin } from './metacoin'
 
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 
-const accountNames = [
-  'coinbase',
-  'Marc Riehm',
-  'David Evans',
-  'Peter Bishop',
-  'Alex Tapscott',
-  'Charlie Evans',
-  'Estelle Evans',
-  'Jonah Group',
-  'Consensys',
-  'R3'
-];
-
 const CADcoin = 68
 
 export default {
@@ -30,7 +17,6 @@ export default {
         let eth = web3.fromWei(web3.eth.getBalance(addresses[i]), 'ether')
         _accounts.push({
           address: addresses[i],
-          name: accountNames[i],
           balance: eth,
           cadCoin: eth * CADcoin,
           meta: await getBalance(addresses[i])
