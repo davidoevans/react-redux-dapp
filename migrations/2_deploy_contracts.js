c4bac1c7534d224364b68ec1e731d98d45e170be
David Evans <david.evans@e-canada.io>
2017-04-11 16:59:22 -0400

    remove deployment of old SimpleWallet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,8 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
-var SimpleWallet = artifacts.require("./SimpleWallet.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
-  deployer.deploy(SimpleWallet);
 };
